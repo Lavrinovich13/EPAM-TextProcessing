6426daf90adc20e8c6fcd78385633531f10fe4e7
Ksenya Lavrinovich <lavrinovich.kg.13@gmail.com>
2015-10-29 23:39:06 +0300

    build collection of IPartOfSentence from string

---
+++ TextProcessing/TextProcessing/Factory/IFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TextProcessing
+{
+    interface IFactory<T, K>
+    {
+        IEnumerable<K> Build(T input);
+    }
+}
