cb9a0558efd5859295bdbd75cd8a39d88c22ddcb
Ksenya Lavrinovich <lavrinovich.kg.13@gmail.com>
2015-10-31 22:09:56 +0300

    no message

--- TextProcessing/TextProcessing/Text.cs
+++ TextProcessing/TextProcessing/Text.cs
@@ -10,6 +10,11 @@ namespace TextProcessing
     {
         private ICollection<ISentence> _Sentences;
 
+        public IEnumerable<ISentence> GetSentences()
+        {
+            return _Sentences.AsEnumerable();
+        }
+
         public Text()
         {
             _Sentences = new List<ISentence>();
@@ -18,6 +23,33 @@ namespace TextProcessing
         public Text(ICollection<ISentence> sentences)
         {
             _Sentences = sentences;
+        }
+
+        public IEnumerable<IWord> GetDistinctWordsBy
+            (Func<ISentence, bool> sentencePredicate, Func<IWord, bool> wordPredicate)
+        {
+            var words = new List<IWord>();
+
+            foreach (var sentence in _Sentences.Where(x => x._Type == SentenceTypes.Exclamatory))
+            {
+                words.AddRange(sentence.GetWordsBy(x => x.Length == 3));
+            }
+
+            return words.GroupBy(x => x._StringValue.ToLowerInvariant()).Select(x => x.First());
+        }
+
+        public void RemoveWordsBy(Func<IWord, bool> predicate)
+        {
+            _Sentences = _Sentences.Select(x => new Sentence(x.RemoveWordsBy(predicate))).ToList<ISentence>();
+        }
+
+        public void ReplaceWordsBy
+            (Func<IWord, bool> predicate, int sentenceIndex, string newString, IFactory<string, IPartOfSentence> partsOfSentenceFactory)
+        {
+            _Sentences.ToList()[sentenceIndex] = 
+                new Sentence(_Sentences
+                    .ElementAt(sentenceIndex)
+                    .ReplaceWordsBy(x => x.Length == 3, newString, partsOfSentenceFactory));
         }
     }
 }
