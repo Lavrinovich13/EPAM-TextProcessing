068b861965efda0a3ab339fbab0016cfd807734f
Ksenya Lavrinovich <lavrinovich.kg.13@gmail.com>
2015-10-31 22:06:19 +0300

    added container of default delimeters

---
+++ TextProcessing/TextProcessing/DelimetersContainer/DefaultDelimeters.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextProcessing
+{
+    class DefaultDelimeters : IDelimetersContainer
+    {
+        public Delimeter _SpaceDelimeter { get; private set; }
+        public Delimeter[] _SentencesDelimeters { get; private set; }
+        public Delimeter[] _WordsDelimeters { get; private set; }
+        public Delimeter[] _WordsConnectionDelimeters { get; private set; }
+        
+
+        public DefaultDelimeters()
+        {
+            _SpaceDelimeter = new Delimeter(" ", false, DelimeterTypes.Single);
+
+            _SentencesDelimeters = new Delimeter[]
+            {
+                new Delimeter(".", false, DelimeterTypes.Single, SentenceTypes.Declarative),
+                new Delimeter("!", false, DelimeterTypes.Single, SentenceTypes.Interrogative),
+                new Delimeter("?", false, DelimeterTypes.Single, SentenceTypes.Exclamatory),
+                new Delimeter("...", false, DelimeterTypes.Single, SentenceTypes.Declarative),
+                new Delimeter("?!", false, DelimeterTypes.Single, SentenceTypes.Exclamatory),
+                new Delimeter("!?", false, DelimeterTypes.Single, SentenceTypes.Exclamatory)
+            };
+
+            _WordsDelimeters = new Delimeter[] 
+            {
+                new Delimeter(",", false, DelimeterTypes.Single),
+                new Delimeter("(", false, DelimeterTypes.FirstInPair),
+                new Delimeter(")", false, DelimeterTypes.LastInPair),
+                new Delimeter("{", false, DelimeterTypes.FirstInPair),
+                new Delimeter("}", false, DelimeterTypes.LastInPair),
+                new Delimeter("[", false, DelimeterTypes.FirstInPair),
+                new Delimeter("]", false, DelimeterTypes.LastInPair),
+                new Delimeter(":", false, DelimeterTypes.Single),
+                new Delimeter(";", false, DelimeterTypes.Single),
+                new Delimeter("'", false, DelimeterTypes.Double),
+                new Delimeter("\"", false, DelimeterTypes.Double),
+                new Delimeter("-", true, DelimeterTypes.Single)
+            };
+
+            _WordsConnectionDelimeters = new Delimeter[] 
+            {
+                new Delimeter("-", false, DelimeterTypes.Single),
+                new Delimeter("'", false, DelimeterTypes.Single)
+            };
+        }
+
+        public Delimeter[] GetDelimetersInSentence()
+        {
+            return _SentencesDelimeters.Concat(_WordsDelimeters).ToArray();
+        }
+    }
+}
---
+++ TextProcessing/TextProcessing/DelimetersContainer/IDelimetersContainer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextProcessing
+{
+    interface IDelimetersContainer
+    {
+        Delimeter _SpaceDelimeter { get; }
+        Delimeter[] _SentencesDelimeters { get; }
+        Delimeter[] _WordsDelimeters { get; }
+        Delimeter[] _WordsConnectionDelimeters { get; }
+        Delimeter[] GetDelimetersInSentence();
+    }
+}
