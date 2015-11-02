035b2b7da36a255f826cde08bfb89165bb5be135
Ksenya Lavrinovich <lavrinovich.kg.13@gmail.com>
2015-11-02 16:42:01 +0300

    made interface internal

--- TextProcessing/TextProcessing/TextItemsInterfaces/IPartOfSentence.cs
+++ TextProcessing/TextProcessing/TextItemsInterfaces/IPartOfSentence.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TextProcessing
 {
-    public interface IPartOfSentence
+    internal interface IPartOfSentence
     {
         string StringValue { get; }
     }
